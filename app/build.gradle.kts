235d671c5c92710eba275bd20ba54f0e24a74cab
ronenanuka6 <ronenanuka6@gmail.com>
2025-02-05 14:35:35 +0200

    add login pages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.surfriders"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.surfriders"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
---
+++ app/src/main/java/com/example/surfriders/modules/login/LoginActivity.kt
@@ -0,0 +1,102 @@
+package com.example.surfriders.modules.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.surfriders.R
+import com.example.surfriders.modules.signup.SignUpActivity
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var emailAddressInputLayout: TextInputLayout
+    private lateinit var emailAddressInputEditText: TextInputEditText
+
+
+    private lateinit var passwordInputLayout: TextInputLayout
+    private lateinit var passwordInputEditText: TextInputEditText
+
+    private lateinit var signUpButton: Button
+    private lateinit var signinButton: Button
+    private lateinit var forgotPasswordButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("creation","creating signing screen ohad")
+        setContentView(R.layout.login_screen)
+        // Set ui widgets
+        setUI()
+    }
+
+    private fun setUI(){
+        signinButton = findViewById(R.id.LoginButton)
+        forgotPasswordButton = findViewById(R.id.ForgotPasswordPageButton)
+        signUpButton = findViewById(R.id.toSignupPageButton)
+
+        signinButton?.setOnClickListener{
+            Log.i("buttonClick","signing button clicked in login page")
+            checkLoginUser()
+        }
+
+        signUpButton?.setOnClickListener{
+            Log.i("buttonClick","signup button in signing screen clicked")
+            val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        forgotPasswordButton?.setOnClickListener{
+            Log.i("buttonClick","Forgot password button in signing screen clicked")
+            val intent = Intent(this@LoginActivity, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun checkLoginUser() {
+
+        emailAddressInputLayout = findViewById(R.id.layoutLoginTextEmail)
+        emailAddressInputEditText = findViewById(R.id.editLoginTextEmailAddress)
+        val emailValue: String = emailAddressInputEditText?.text.toString().trim()
+
+        passwordInputEditText = findViewById(R.id.editLoginTextPassword)
+        passwordInputLayout = findViewById(R.id.layoutLoginTextPassword)
+        val passwordValue: String = passwordInputEditText?.text.toString().trim()
+
+        val checkUserValidation = loginUserValidation(emailValue,passwordValue)
+
+        if(checkUserValidation) {
+            Log.i("buttonClick", "signIn button in signing screen clicked")
+            Log.i("signingSubmit", "email input is: $emailValue")
+            Log.i("signingSubmit", "password Input is: $passwordValue")
+
+        }
+    }
+
+    private fun loginUserValidation(
+        email: String,
+        password: String
+    ):Boolean {
+
+        // Basic checks
+        if (email.isEmpty()) {
+            emailAddressInputLayout.error = "Email cannot be empty"
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailAddressInputLayout.error = "Invalid email format"
+            return false
+        } else {
+            emailAddressInputLayout.error = null
+        }
+        if (password.isEmpty()) {
+            passwordInputLayout.error = "Password cannot be empty"
+            return false
+        } else {
+            passwordInputLayout.error = null
+        }
+        return true
+
+    }
+}
